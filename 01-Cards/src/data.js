23ca0f76b52211aefef59a1bb2b14e9d05bf8f3b
ketanmakwana30 <ketanmakwana0130@gmail.com>
2026-06-01 08:27:42 +0530

    add cards project

---
+++ 01-Cards/src/data.js
@@ -0,0 +1,24 @@
+const profile = [
+    {
+        id: 1,
+        name: "ketan",
+        role: "Frontend dev",
+        image:"https://images.unsplash.com/flagged/photo-1595514191830-3e96a518989b?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 2,
+        name: "Rajvir",
+        role: "Backend dev",
+        image:"https://images.unsplash.com/photo-1594377477332-c127ccb1f4a3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHByb2ZpbGUlMjBwaWNzfGVufDB8fDB8fHww"
+    },
+    {
+        id: 3,
+        name: "Aniket",
+        role: "AI/ ML",
+        image:"https://plus.unsplash.com/premium_photo-1726862586698-4404605ac267?q=80&w=750&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+
+]
+
+
+export default profile
